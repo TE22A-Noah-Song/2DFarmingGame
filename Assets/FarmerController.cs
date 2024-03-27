9999bd2f08c137f21ffda07a24b3c459e3f6cad4
Noah Söng <noah.song@elev.ga.ntig.se>
2024-03-27 08:28:14 +0100

    fixat collider, rigidbody och så att man kan gå snett utan bugg

--- Assets/FarmerController.cs
+++ Assets/FarmerController.cs
@@ -11,13 +11,13 @@ public class FarmerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    float moveX = Input.GetAxisRaw("Horizontal");
+        float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         Vector2 movementX = new Vector2(moveX, 0);
@@ -27,7 +27,7 @@ public class FarmerController : MonoBehaviour
 
         transform.Translate(movement * speed * Time.deltaTime);
 
-        if (Mathf.Abs(transform.position.x) > 7.3f)
+        if (Mathf.Abs(transform.position.x) > 7.4f)
         {
             transform.Translate(-movementX * speed * Time.deltaTime);
         }
@@ -35,31 +35,31 @@ public class FarmerController : MonoBehaviour
         if (Mathf.Abs(transform.position.y) > 3.2f)
         {
             transform.Translate(-movementY * speed * Time.deltaTime);
-        } 
+        }
 
-         if (moveY > 0)
-            {
-                animator.Play("WalkUp");
-            }
+        if (moveY > 0)
+        {
+            animator.Play("WalkUp");
+        }
 
-            else if (moveY < 0)
-            {
-                animator.Play("WalkDown");
-            }
+        else if (moveY < 0)
+        {
+            animator.Play("WalkDown");
+        }
 
-            if (moveY == 0 && moveX == 0)
-            {
-                animator.Play("Idle");
-            }
+        else if (moveY == 0 && moveX == 0)
+        {
+            animator.Play("Idle");
+        }
 
-            else if (moveX < 0)
-            {
-                animator.Play("WalkLeft");
-            }
+        else if (moveX < 0)
+        {
+            animator.Play("WalkLeft");
+        }
 
-            else if (moveX > 0)
-            {
-                animator.Play("WalkRight");
-            }      
+        else if (moveX > 0)
+        {
+            animator.Play("WalkRight");
+        }
     }
 }
