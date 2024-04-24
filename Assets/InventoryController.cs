b7a19bf8dd1ee7866488a0376258f7c3b4110416
Noah Söng <noah.song@elev.ga.ntig.se>
2024-04-24 08:55:42 +0200

    klart med inventory select

--- Assets/InventoryController.cs
+++ Assets/InventoryController.cs
@@ -3,45 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class InventoryController : MonoBehaviour
 {
     public Image[] InventorySlots;
     public Sprite RedSquareSprite;
-
     private int selectedSlotIndex = -1;
 
     void Update()
     {
-        // Check for input to select slots
+        // kolla input för vald slot
         for (int i = 0; i < InventorySlots.Length; i++)
         {
             if (Input.GetKeyDown(KeyCode.Alpha1 + i))
             {
                 SelectSlot(i);
-                break; // Exit loop after selecting a slot
+                break; // gå ut ur loopen efter vald slot
             }
         }
     }
 
     void SelectSlot(int index)
     {
-        // Deselect previous slot if one was selected
+        // Ta bort tidigare vald slot
         if (selectedSlotIndex != -1)
         {
-            // Reset color only if it's not the currently selected slot
+            // åteställ färg
             if(selectedSlotIndex != index)
             {
                 InventorySlots[selectedSlotIndex].color = Color.white;
             }
         }
 
-        // Select new slot
+        // Välj nya slots
         if (index >= 0 && index < InventorySlots.Length)
         {
-            InventorySlots[index].color = Color.red; // Highlight the selected slot
+            InventorySlots[index].color = Color.red; // Markera den valda sloten med röd färg
             selectedSlotIndex = index;
 
-            // Debug selected slot
+            // Debugga vald slot
             Debug.Log("Selected slot " + (index + 1));
         }
     }
