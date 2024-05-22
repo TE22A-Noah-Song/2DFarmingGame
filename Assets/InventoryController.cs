c5afaff7f79f218c7b1a90d8eb57600bbc777d15
Noah Söng <noah.song@elev.ga.ntig.se>
2024-05-22 18:49:08 +0200

    metod för att visa vald verktyg i handen i scenen

--- Assets/InventoryController.cs
+++ Assets/InventoryController.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class InventoryController : MonoBehaviour
 {
     public Image[] InventorySlots;
-    public Sprite RedSquareSprite;
+    public GameObject[] Tools; // Array för att hålla referenser till verktygsobjekt
+    public Transform toolHolder; // Transform där verktyget ska visas
     private int selectedSlotIndex = -1;
+    private GameObject currentTool;
+
 
     void Update()
     {
@@ -43,6 +46,27 @@ public class InventoryController : MonoBehaviour
 
             // Debugga vald slot
             Debug.Log("Selected slot " + (index + 1));
+        }
+    }
+
+    void UpdateHeldTool()
+    {
+        // Först, ta bort det aktuella verktyget om det finns ett
+        if (currentTool != null)
+        {
+            Destroy(currentTool);
+        }
+
+        // Om ingen slot är vald, avsluta funktionen
+        if (selectedSlotIndex == -1) return;
+
+        // Hämta verktyget för den valda sloten
+        GameObject toolToEquip = Tools[selectedSlotIndex];
+
+        // Om det finns ett verktyg för denna slot, skapa det
+        if (toolToEquip != null)
+        {
+            currentTool = Instantiate(toolToEquip, toolHolder.position, toolHolder.rotation, toolHolder);
         }
     }
 }
